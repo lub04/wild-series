1566a924f5c65ed1193ae7b6afc7d02ddc90d012
Lubin Chauvreau <lubin-chauvreau@laposte.net>
2024-06-24 16:22:13 +0200

    mise en place du shéma sql et création de la db

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,12 +1,15 @@
-create table user (
-  id int unsigned primary key auto_increment not null,
-  email varchar(255) not null unique,
-  password varchar(255) not null
+create table category (
+  id int  primary key auto_increment not null,
+  name varchar(255) not null
 );
 
-create table item (
+create table program (
   id int unsigned primary key auto_increment not null,
   title varchar(255) not null,
-  user_id int unsigned not null,
-  foreign key(user_id) references user(id)
+  synopsis TEXT NOT NULL,
+  poster VARCHAR(255)NOT NULL,
+  country VARCHAR(255)NOT NULL,
+  year int NOT NULL,
+  category_id int not null,
+  foreign key(category_id) references category(id)
 );
